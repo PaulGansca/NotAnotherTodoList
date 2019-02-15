17bb4200b29af2bdae6756bdb4007c1a74b76025
Paul Gansca <gansca.paul@gmail.com>
2019-02-15 16:48:11 +0000

    Connected to db, created item Schema

--- app.js
+++ app.js
@@ -2,27 +2,58 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
-const date = require(__dirname + "/date.js");
+const mongoose = require("mongoose");
 
 const app = express();
 
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.use(express.static("public"));
+mongoose.connect("mongodb://localhost:27017/todolistDB", {
+  useNewUrlParser: true
+});
 
-const items = ["Buy Food", "Cook Food", "Eat Food"];
-const workItems = [];
+const itemsSchema = {
+  name: String
+};
 
-app.get("/", function(req, res) {
+const Item = mongoose.model("Item", itemsSchema);
 
-const day = date.getDate();
+const item1 = new Item({
+  name: "Make todo list persist"
+});
 
-  res.render("list", {listTitle: day, newListItems: items});
+const item2 = new Item({
+  name: "Celebrate exam results"
+});
+
+const item3 = new Item({
+  name: "Revise Language Processors"
+});
+
+const defaultItems = [item1, item2, item3];
+
+Item.insertMany(defaultItems, err => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("Saved default items to db")
+  }
+})
+
+app.get("/", function (req, res) {
+
+  res.render("list", {
+    listTitle: "Today",
+    newListItems: items
+  });
 
 });
 
-app.post("/", function(req, res){
+app.post("/", function (req, res) {
 
   const item = req.body.newItem;
 
@@ -35,14 +66,17 @@ app.post("/", function(req, res){
   }
 });
 
-app.get("/work", function(req,res){
-  res.render("list", {listTitle: "Work List", newListItems: workItems});
+app.get("/work", function (req, res) {
+  res.render("list", {
+    listTitle: "Work List",
+    newListItems: workItems
+  });
 });
 
-app.get("/about", function(req, res){
+app.get("/about", function (req, res) {
   res.render("about");
 });
 
-app.listen(3000, function() {
+app.listen(3000, function () {
   console.log("Server started on port 3000");
 });
