2f32b77cca43219e2608b6898b94cd9fa4bc62f0
Paul Gansca <gansca.paul@gmail.com>
2019-03-02 09:39:09 +0200

    Kebab case list names, fix deprecation issue on delete route

--- app.js
+++ app.js
@@ -7,35 +7,38 @@ const _ = require("lodash");
 
 const app = express();
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
+app.use(
+    bodyParser.urlencoded({
+        extended: true
+    })
+);
 app.use(express.static("public"));
-app.set('views', __dirname + '/views');
+app.set("views", __dirname + "/views");
 mongoose.connect("mongodb://localhost:27017/todolistDB", {
-  useNewUrlParser: true
+    useNewUrlParser: true,
+    useFindAndModify: false
 });
 
 const itemsSchema = {
-  name: String
+    name: String
 };
 
-const ROOT_LIST = "Today";
+const ROOT_LIST = "All Lists";
 
 const Item = mongoose.model("Item", itemsSchema);
 
 const item1 = new Item({
-  name: "Make todo list persist"
+    name: "Make todo list persist"
 });
 
 const item2 = new Item({
-  name: "Celebrate exam results"
+    name: "Celebrate exam results"
 });
 
 const item3 = new Item({
-  name: "Revise Language Processors"
+    name: "Revise Language Processors"
 });
 
 const defaultItems = [item1, item2, item3];
@@ -43,193 +46,167 @@ const defaultItems = [item1, item2, item3];
 //ideas
 
 const ideasSchema = {
-  name: String,
-  content: String
-}
+    name: String,
+    content: String
+};
 
 const Idea = mongoose.model("Idea", ideasSchema);
 
 //clipboard
 
 const linksSchema = {
-  name: String,
-  clipboard: String
-}
+    name: String,
+    clipboard: String
+};
 
 const Link = mongoose.model("Link", linksSchema);
 
 const listSchema = {
-  name: String,
-  type: String,
-  description: String,
-  todos: [itemsSchema],
-  ideas: [ideasSchema],
-  clipboard: [linksSchema]
+    name: String,
+    type: String,
+    description: String,
+    todos: [itemsSchema],
+    ideas: [ideasSchema],
+    clipboard: [linksSchema]
 };
 
 const List = mongoose.model("List", listSchema);
 
-app.get("/", function (req, res) {
-
-  List.find((err, results) => {
-    if (err) {
-      console.log(err);
-    } else {
-      res.render("home", {
-        listTitle: ROOT_LIST,
-        newLists: results
-      });
-    }
-  })
-
+app.get("/", function(req, res) {
+    List.find((err, results) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.render("home", {
+                listTitle: ROOT_LIST,
+                newLists: results
+            });
+        }
+    });
 });
 
 app.get("/createList", (req, res) => {
-  res.render("createList");
+    res.render("createList");
 });
 
 app.post("/createList", (req, res) => {
-  let listName = _.kebabCase(req.body.listName);
-  let listType = req.body.listType;
-  let listDescription = req.body.listDescription;
+    let listName = _.kebabCase(req.body.listName);
+    let listType = req.body.listType;
+    let listDescription = req.body.listDescription;
 
-  List.findOne({
-    name: listName
-  }, (err, results) => {
-    if (err) {
-      res.send(err);
-    } else if (!results) {
-      //list doesn't exist so create one
-      const list = new List({
-        name: listName,
-        type: listType,
-        description: listDescription,
-      });
+    List.findOne(
+        {
+            name: listName
+        },
+        (err, results) => {
+            if (err) {
+                res.send(err);
+            } else if (!results) {
+                //list doesn't exist so create one
+                const list = new List({
+                    name: listName,
+                    type: listType,
+                    description: listDescription
+                });
 
-      list.save();
+                list.save();
 
-      res.redirect(`/${listName}`);
-    } else {
-      //show existing
-      res.redirect(`/${listName}`)
-      res.render(listType, {
-        listTitle: results.name,
-        newListItems: results.todos
-      });
-    }
-  })
-
-});
-
-app.get("/create/:customListName", (req, res) => {
-  let customListName = req.params.customListName;
-
-  List.findOne({
-    name: customListName
-  }, (err, results) => {
-    if (err) {
-      console.log(err);
-    } else if (!results) {
-      //list doesn't exist so create one
-      const list = new List({
-        name: customListName,
-        type: "HARDCODED",
-        description: "HARDCODED IN CREATE ROUTE",
-        todos: defaultItems
-      });
-
-      list.save();
-
-      res.redirect(`/${customListName}`)
-    } else {
-      //show existing
-      res.redirect(`/${customListName}`)
-      res.render("todolist", {
-        listTitle: results.name,
-        newListItems: results.todos
-      });
-    }
-  })
-
+                res.redirect(`/${listName}`);
+            } else {
+                //show existing
+                res.redirect(`/${listName}`);
+                res.render(listType, {
+                    listTitle: results.name,
+                    newListItems: results.todos
+                });
+            }
+        }
+    );
 });
 
 app.get("/:customListName", (req, res) => {
-  let customListName = _.kebabCase(req.params.customListName);
+    let customListName = _.kebabCase(req.params.customListName);
 
-  List.findOne({
-    name: customListName
-  }, (err, results) => {
-    if (err) {
-      console.log(err);
-    } else {
-      //show existing
-      res.render("todolist", {
-        listTitle: results.name,
-        newListItems: results.todos
-      });
-    }
-  })
-})
+    List.findOne(
+        {
+            name: customListName
+        },
+        (err, results) => {
+            if (err) {
+                console.log(err);
+            } else {
+                //show existing
+                res.render("todolist", {
+                    listTitle: results.name,
+                    newListItems: results.todos
+                });
+            }
+        }
+    );
+});
 
-app.post("/", function (req, res) {
+app.post("/", function(req, res) {
+    const itemName = req.body.newItem;
+    const listName = req.body.list;
 
-  const itemName = req.body.newItem;
-  const listName = req.body.list;
+    const item = new Item({
+        name: itemName
+    });
 
-  const item = new Item({
-    name: itemName
-  });
-
-  if (listName === ROOT_LIST) {
-    item.save();
-    res.redirect("/");
-  } else {
-    List.findOne({
-      name: listName
-    }, (err, results) => {
-      results.todos.push(item);
-      results.save();
-      res.redirect(`/${listName}`);
-    })
-  }
-
+    List.findOne(
+        {
+            name: listName
+        },
+        (err, results) => {
+            results.todos.push(item);
+            results.save();
+            res.redirect(`/${listName}`);
+        }
+    );
 });
 
 app.post("/delete", (req, res) => {
-  //select inputs that are checked
-  const doneItem = req.body.checkbox;
-  const listName = req.body.listName;
+    //select inputs that are checked
+    const doneItem = req.body.checkbox;
+    const listName = req.body.listName;
 
-  //use their name to find them in the db and delete
-  if (listName === ROOT_LIST) {
-    List.deleteOne({
-      _id: doneItem
-    }, err => {
-      if (!err) {
-        res.redirect("/");
-      }
-    });
-  } else {
-    List.findOneAndUpdate({
-      name: listName
-    }, {
-      $pull: {
-        todos: {
-          _id: doneItem
-        }
-      }
-    }, (err, results) => {
-      if (!err) {
-        res.redirect(`/${listName}`)
-      }
-    })
-  }
+    //use their name to find them in the db and delete
+    if (listName === ROOT_LIST) {
+        List.deleteOne(
+            {
+                _id: doneItem
+            },
+            err => {
+                if (!err) {
+                    res.redirect("/");
+                }
+            }
+        );
+    } else {
+        List.findOneAndUpdate(
+            {
+                name: listName
+            },
+            {
+                $pull: {
+                    todos: {
+                        _id: doneItem
+                    }
+                }
+            },
+            (err, results) => {
+                if (!err) {
+                    res.redirect(`/${listName}`);
+                }
+            }
+        );
+    }
 });
 
-app.get("/about", function (req, res) {
-  res.render("about");
+app.get("/about", function(req, res) {
+    res.render("about");
 });
 
-app.listen(3000, function () {
-  console.log("Server started on port 3000");
+app.listen(3000, function() {
+    console.log("Server started on port 3000");
 });
